50aca80a118bb4b8c5061bb105e00a0530cb7ee4
quack79 <alex@quack.me>
2024-08-27 12:53:37 +0100

    Update view.blade.php

--- resources/views/hardware/view.blade.php
+++ resources/views/hardware/view.blade.php
@@ -245,7 +245,7 @@
                                                       'id' => 'bulkForm']) }}
                                     <input type="hidden" name="bulk_actions" value="labels" />
                                     <input type="hidden" name="ids[{{$asset->id}}]" value="{{ $asset->id }}" />
-                                    <button class="btn btn-block btn-social btn-sm btn-default" id="bulkEdit"{{ (!$asset->model ? ' disabled' : '') }}{!! (!$asset->model ? ' data-tooltip="true" title="'.trans('admin/hardware/general.model_invalid').'"' : '') !!}>
+                                    <button type="submit" formtarget="_blank" class="btn btn-block btn-social btn-sm btn-default" id="bulkEdit"{{ (!$asset->model ? ' disabled' : '') }}{!! (!$asset->model ? ' data-tooltip="true" title="'.trans('admin/hardware/general.model_invalid').'"' : '') !!}>
                                         <x-icon type="assets" />
                                         {{ trans_choice('button.generate_labels', 1) }}</button>
                                         {{ Form::close() }}
@@ -1066,189 +1066,12 @@
                                             {{ ($asset->userRequests) ? (int) $asset->userRequests->count() : '0' }}
                                         </div>
                                     </div>
-
-                                    <div class="row">
-                                        <div class="col-md-2">
-                                            <strong>
-                                               Labels
-                                            </strong>
-                                        </div>
-                                        <div class="col-md-6">
-                                            {{ Form::open([
-                                                      'method' => 'POST',
-                                                      'route' => ['hardware/bulkedit'],
-                                                      'class' => 'form-inline',
-                                                       'id' => 'bulkForm']) }}
-                                                <input type="hidden" name="bulk_actions" value="labels" />
-                                                <input type="hidden" name="ids[{{$asset->id}}]" value="{{ $asset->id }}" />
-                                                <button type="submit" formtarget="_blank" class="btn btn-sm btn-default" id="bulkEdit"{{ (!$asset->model ? ' disabled' : '') }}{!! (!$asset->model ? ' data-tooltip="true" title="'.trans('admin/hardware/general.model_invalid').'"' : '') !!}><i class="fas fa-barcode" aria-hidden="true"></i> {{ trans_choice('button.generate_labels', 1) }}</button>
-
-                                            {{ Form::close() }}
-
-                                        </div>
-                                    </div>
-                                </div> <!-- end row-striped -->
-
-                            </div><!-- /col-md-8 -->
-
-                            <div class="col-md-4">
-
-                                @if (($asset->image) || (($asset->model) && ($asset->model->image!='')))
-                                    <div class="text-center col-md-12" style="padding-bottom: 15px;">
-                                        <a href="{{ ($asset->getImageUrl()) ? $asset->getImageUrl() : null }}" data-toggle="lightbox">
-                                            <img src="{{ ($asset->getImageUrl()) ? $asset->getImageUrl() : null }}" class="assetimg img-responsive" alt="{{ $asset->getDisplayNameAttribute() }}">
-                                        </a>
-                                    </div>
-                                @else
-                                    <!-- generic image goes here -->
-                                @endif
-
-                                    <!-- Start side button column -->
-
-                                    @if (($asset->assetstatus) && ($asset->assetstatus->deployable=='1'))
-                                        @if (($asset->assigned_to != '') && ($asset->deleted_at==''))
-                                            @can('checkin', \App\Models\Asset::class)
-                                                <div class="col-md-12">
-                                                    <span class="tooltip-wrapper"{!! (!$asset->model ? ' data-tooltip="true" title="'.trans('admin/hardware/general.model_invalid_fix').'"' : '') !!}>
-                                                        <a role="button" href="{{ route('hardware.checkin.create', $asset->id) }}" class="btn btn-sm btn-primary btn-block hidden-print{{ (!$asset->model ? ' disabled' : '') }}">
-                                                        {{ trans('admin/hardware/general.checkin') }}
-                                                    </a>
-                                                    </span>
-                                                </div>
-                                            @endcan
-                                        @elseif (($asset->assigned_to == '') && ($asset->deleted_at==''))
-                                            @can('checkout', \App\Models\Asset::class)
-                                                <div class="col-md-12" style="padding-top: 5px;">
-                                                    <span class="tooltip-wrapper"{!! (!$asset->model ? ' data-tooltip="true" title="'.trans('admin/hardware/general.model_invalid_fix').'"' : '') !!}>
-                                                        <a href="{{ route('hardware.checkout.create', $asset->id)  }}" class="btn btn-sm btn-primary btn-block hidden-print{{ (!$asset->model ? ' disabled' : '') }}">
-                                                        {{ trans('admin/hardware/general.checkout') }}
-                                                    </a>
-                                                    </span>
-                                                </div>
-                                            @endcan
-                                        @endif
-                                    @endif
-
-
-                                    @can('update', $asset)
-                                        @if ($asset->deleted_at=='')
-                                        <div class="col-md-12" style="padding-top: 5px;">
-                                            <a href="{{ route('hardware.edit', $asset->id) }}" class="btn btn-sm btn-primary btn-block hidden-print">
-                                                {{ trans('admin/hardware/general.edit') }}
-                                            </a>
-                                        </div>
-                                        @endif
-                                    @endcan
-
-                                    @can('create', $asset)
-                                        <div class="col-md-12" style="padding-top: 5px;">
-                                            <a href="{{ route('clone/hardware', $asset->id) }}" class="btn btn-sm btn-primary btn-block hidden-print">
-                                                {{ trans('admin/hardware/general.clone') }}
-                                            </a>
-                                        </div>
-                                    @endcan
-
-                                    @can('audit', \App\Models\Asset::class)
-                                        <div class="col-md-12" style="padding-top: 5px;">
-                                            <span class="tooltip-wrapper"{!! (!$asset->model ? ' data-tooltip="true" title="'.trans('admin/hardware/general.model_invalid_fix').'"' : '') !!}>
-                                                <a href="{{ route('asset.audit.create', $asset->id)  }}" class="btn btn-sm btn-primary btn-block hidden-print{{ (!$asset->model ? ' disabled' : '') }}">
-                                                 {{ trans('general.audit') }}
-                                            </a>
-                                            </span>
-                                        </div>
-                                    @endcan
-
-                                    @can('delete', $asset)
-                                        <div class="col-md-12" style="padding-top: 30px; padding-bottom: 30px;">
-                                            @if ($asset->deleted_at=='')
-                                                <button class="btn btn-sm btn-block btn-danger delete-asset" data-toggle="modal" data-title="{{ trans('general.delete') }}" data-content="{{ trans('general.sure_to_delete_var', ['item' => $asset->asset_tag]) }}" data-target="#dataConfirmModal">{{ trans('general.delete') }}
-                                                </button>
-                                                <span class="sr-only">{{ trans('general.delete') }}</span>
-                                            @else
-                                                <form method="POST" action="{{ route('restore/hardware', ['assetId' => $asset->id]) }}">
-                                                    @csrf
-                                                    <button class="btn btn-sm btn-warning btn-block">{{ trans('general.restore') }}</button>
-                                                </form>
-                                           @endif
-                                        </div>
-                                    @endcan
-
-                                @if (($asset->assignedTo) && ($asset->deleted_at==''))
-                                    <div style="text-align: left">
-                                        <h2>{{ trans('admin/hardware/form.checkedout_to') }}</h2>
-                                        <p>
-                                        @if($asset->checkedOutToUser()) <!-- Only users have avatars currently-->
-                                            <img src="{{ $asset->assignedTo->present()->gravatar() }}" class="user-image-inline" alt="{{ $asset->assignedTo->present()->fullName() }}">
-                                            @endif
-                                        </p>
-                                            {!! $asset->assignedTo->present()->glyph() . ' ' .$asset->assignedTo->present()->nameUrl() !!}
-                                        </p>
-
-                                        <ul class="list-unstyled" style="line-height: 25px;">
-                                            @if ((isset($asset->assignedTo->email)) && ($asset->assignedTo->email!=''))
-                                                <li>
-                                                    <i class="far fa-envelope" aria-hidden="true"></i>
-                                                    <a href="mailto:{{ $asset->assignedTo->email }}">{{ $asset->assignedTo->email }}</a>
-                                                </li>
-                                            @endif
-
-                                            @if ((isset($asset->assignedTo)) && ($asset->assignedTo->phone!=''))
-                                                <li>
-                                                    <i class="fas fa-phone" aria-hidden="true"></i>
-                                                    <a href="tel:{{ $asset->assignedTo->phone }}">{{ $asset->assignedTo->phone }}</a>
-                                                </li>
-                                            @endif
-
-                                            @if((isset($asset->assignedTo)) && ($asset->assignedTo->department))
-                                                <li>{{ trans('admin/hardware/general.user_department') }}: {{ $asset->assignedTo->department->name}}</li>
-                                            @endif
-
-                                            @if (isset($asset->location))
-                                                <li><i class="fas fa-map-marker-alt" aria-hidden="true"></i> {{ $asset->location->name }}</li>
-                                                <li>{{ $asset->location->address }}
-                                                    @if ($asset->location->address2!='')
-                                                        {{ $asset->location->address2 }}
-                                                    @endif
-                                                </li>
-
-                                                <li>{{ $asset->location->city }}
-                                                    @if (($asset->location->city!='') && ($asset->location->state!=''))
-                                                        ,
-                                                    @endif
-                                                    {{ $asset->location->state }} {{ $asset->location->zip }}
-                                                </li>
-                                            @endif
-                                                <li>
-                                                    <i class="fas fa-calendar"></i> {{ trans('admin/hardware/form.checkout_date') }}: {{ Helper::getFormattedDateObject($asset->last_checkout, 'date', false) }}
-                                                </li>
-                                            @if (isset($asset->expected_checkin))
-                                                <li>
-                                                    <i class="fas fa-calendar"></i> {{ trans('admin/hardware/form.expected_checkin') }}: {{ Helper::getFormattedDateObject($asset->expected_checkin, 'date', false) }}
-                                                </li>
-                                            @endif
-                                        </ul>
-                                    </div>
-
-                                @endif
-
-                                @if  ($snipeSettings->qr_code=='1')
-                                    <div class="col-md-12" style="padding-top: 15px;">
-                                        <img src="{{ config('app.url') }}/hardware/{{ $asset->id }}/qr_code" class="img-thumbnail pull-right" style="height: 100px; width: 100px; margin-right: 10px;" alt="QR code for {{ $asset->getDisplayNameAttribute() }}">
-                                    </div>
-                                @endif
-
-                            </div> <!-- div.col-md-4 -->
-                        </div><!-- /row -->
-                    </div><!-- /.tab-pane asset details -->
-                 
-
                                     
                                 </div> <!--/end striped container-->
                             </div> <!-- end col-md-9 -->
                         </div><!-- end info-stack-container -->
                         </div> <!--/.row-->
                     </div><!-- /.tab-pane -->
-
 
                     <div class="tab-pane fade" id="software">
                         <div class="row{{($asset->licenses->count() > 0 ) ? '' : ' hidden-print'}}">
